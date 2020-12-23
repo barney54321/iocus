a2bfa642eabb5723298c5cd5f45fdcb3bfbcafca
Andrew Esteban <andrewesteban1999@gmail.com>
2020-12-23 17:17:39 +1100

    moving bird

--- iocus/iocus.js
+++ iocus/iocus.js
@@ -1,30 +1,72 @@
 
 class GameObject {
-    constructor(x=0, y=0, visible=true, layer=0) {
+    constructor(x=0, y=0, visible=true, layer=0, src=null) {
         this.x = x;
         this.y = y;
+
+        this.xVel = 0;
+        this.yVel = 0;
+
         this.visible = visible;
         this.layer = layer;
-        this.behaviour = () => {};
 
         this.image = new Image(40, 40);
-        this.image.src = "images/sample.png";
+        this.image.src = src;
+
+        this.pressedKeys = [];
+
+        window.addEventListener("keydown", this.keyDownEvent);
+        window.addEventListener("keyup", this.keyUpEvent);
     }
 
     // Draw the object to the screen
-    draw(context) {
+    draw = (context) => {
         if (this.visible) {
             context.drawImage(this.image, this.x, this.y);
         }
     }
 
     // Update the object every frame
-    tick() {
-        this.behaviour();
+    tick = () => {
+
     }
 
-    setBehaviour(behaviour) {
-        this.behaviour = behaviour;
+    supertick = () => {
+        this.pressedKeys.forEach(x => this.keyHold(x));
+
+        this.x += this.xVel;
+        this.y += this.yVel;
+        
+        this.tick();
+    }
+
+    // Called when key is logged as being pressed
+    keyDownEvent = (event) => {
+        if (this.pressedKeys.indexOf(event.keyCode) < 0) {
+            this.keyDown(event.keyCode);
+            this.pressedKeys.push(event.keyCode);
+        }
+    }
+
+    // Called when key is released
+    keyUpEvent = (event) => {
+        this.pressedKeys = this.pressedKeys.filter((x) => x != event.keyCode);
+        this.keyUp(event.keyCode);
+    }
+
+    // Called when key is pressed down
+    keyDown(keyCode) {
+
+    }
+
+    // Called while key is held down
+    keyHold(keyCode) {
+
+    }
+
+    // Called when key is released
+    keyUp(keyCode) {
+
     }
 }
 
@@ -35,14 +77,38 @@ class Game {
         this.objects = [];
         this.framerate = framerate;
 
-        this.keydownEvents = {};
+        this.pressedKeys = [];
 
-        this.keydown = (event) => {
-            console.log(event.keyCode);
-            this.keydownEvents[event.keyCode]();
-        };
+        window.addEventListener("keydown", this.keyDownEvent);
+        window.addEventListener("keyup", this.keyUpEvent);
+    }
 
-        window.addEventListener("keydown", this.keydown);
+    // Called when key is logged as being pressed
+    keyDownEvent = (event) => {
+        if (this.pressedKeys.indexOf(event.keyCode) < 0) {
+            this.keyDown(event.keyCode);
+            this.pressedKeys.push(event.keyCode);
+        }
+    }
+
+    // Called when key is released
+    keyUpEvent = (event) => {
+        this.pressedKeys = this.pressedKeys.filter((x) => x != event.keyCode);
+
+        this.keyUp(event.keyCode);
+    }
+
+    // Called when key is pressed down
+    keyDown(keyCode) {
+
+    }
+
+    // Called while key is held down
+    keyHold(keyCode) {
+    }
+
+    // Called when key is released
+    keyUp(keyCode) {
     }
 
     addObject = (object) => {
@@ -54,12 +120,8 @@ class Game {
         this.objects = this.objects.filter(x => x != object);
     }
 
-    addKeyDown = (keyCode, fun) => {
-        this.keydownEvents[keyCode] = fun;
-    }
-
     tick = () => {
-        this.objects.forEach((obj) => obj.tick());
+        
     }
 
     draw = () => {
@@ -68,6 +130,8 @@ class Game {
     }
 
     update = () => {
+        this.pressedKeys.forEach(x => this.keyHold(x));
+        this.objects.forEach((obj) => obj.supertick());
         this.tick();
         this.draw();
     }
--- sample/main.js
+++ sample/main.js
@@ -1,20 +1,46 @@
 
-var game = new Game("game", 30);
+class Box extends GameObject {
+    constructor(x=0, y=0, visible=true, layer=0) {
+        super(x, y, visible, layer, "images/sample.png");
 
-var box = new GameObject(10, 10, true, 10);
+        this.horizontalSpeed = 1;
+    }
 
-box.setBehaviour(() => {
-    box.x += 0.5;
-});
+    tick = () => {
+        
+    }
+
+    keyHold(keyCode) {
+        if (keyCode === 39) {
+            this.xVel = this.horizontalSpeed;
+        }
+
+        if (keyCode === 37) {
+            this.xVel = -this.horizontalSpeed;
+        }
+    }
+
+    keyUp(keyCode) {
+        if (keyCode === 39) {
+            this.xVel = 0;
+        }
+
+        if (keyCode === 37) {
+            this.xVel = 0;
+        }
+    }
+}
+
+class Manager extends Game {
+    constructor(id, framerate=60) {
+        super(id, framerate);
+    }
+}
+
+var game = new Manager("game", 30);
+
+var box = new Box(10, 10, true, 10);
 
 game.addObject(box);
-
-game.addKeyDown(40, () => {
-    box.y += 10;
-})
-
-game.addKeyDown(38, () => {
-    box.y -= 10;
-})
 
 game.run();
