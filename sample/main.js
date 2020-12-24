5d52b425c1e5f18a2f38067170c58caa1775acc4
Andrew Esteban <andrewesteban1999@gmail.com>
2020-12-24 12:28:57 +1100

    added pipes to demo

--- sample/main.js
+++ sample/main.js
@@ -1,11 +1,12 @@
-class Box extends PhysicsObject {
+class Bird extends PhysicsObject {
     constructor(x=0, y=0, visible=true, layer=0) {
-        super(x, y, visible, layer, "images/sample.png");
+        super(x, y, visible, layer, "images/bird.png");
 
         this.horizontalSpeed = 1;
+        this.gravity = 0.6;
 
-        this.width = 60;
-        this.height = 60;
+        this.width = 30;
+        this.height = 25;
     }
 
     keyDown(keyCode) {
@@ -15,13 +16,58 @@ class Box extends PhysicsObject {
     }
 }
 
+class Pipe extends PhysicsObject {
+    constructor() {
+        super(700, 150, true, 0, "images/pipe.png");
+        this.gravity = 0;
+
+        this.y = Math.random() * 250 + 100
+
+        this.xVel = -5;
+
+        this.width = 52;
+        this.height = 320;
+    }
+}
+
+class DownPipe extends PhysicsObject {
+    constructor(partner) {
+        super(partner.x, partner.y - 400, true, 0, "images/down_pipe.png");
+
+        this.gravity = partner.gravity;
+
+        this.xVel = partner.xVel;
+
+        this.width = partner.width;
+        this.height = partner.height;
+    }
+}
+
 class Manager extends Game {
     constructor(id, framerate=60) {
         super(id, framerate);
 
-        var box = new Box(10, 10, true, 10);
+        var box = new Bird(50, 10, true, 10);
 
         this.addObject(box);
+
+        this.counter = 0;
+        this.timeBetweenPipes = 50;
+    }
+
+    tick = () => {
+        this.counter++;
+
+        if (this.counter == this.timeBetweenPipes) {
+            this.counter = 0;
+
+            // Create new pipe
+            var pipe = new Pipe();
+            var downPipe = new DownPipe(pipe);
+
+            this.addObject(pipe);
+            this.addObject(downPipe);
+        }
     }
 }
 
